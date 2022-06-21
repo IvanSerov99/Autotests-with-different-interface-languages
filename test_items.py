40743da9e703bebbfebd6b7ee397f5a2cdb84786
Serov Ivan <ISerov@croc.ru>
2022-06-21 21:54:04 +0300

    Добален таймер на 30 секунд для вашей проверки

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
 import time
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,10 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 class Test_shop():
 
     @pytest.mark.test_button
-    def test_button(self, browser):
+    def test_goods_has_add_to_basket_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.implicitly_wait(15)
         browser.get(link)
+        #sleep(30) #sleep для проверки правильности инициализации браузера
         button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
             (By.CSS_SELECTOR, "form.add-to-basket#add_to_basket_form button.btn.btn-lg.btn-primary.btn-add-to-basket")))
         assert (button!= None, 'Кнопка не найдена')
