7cff42e47ee5414a6c4ba09b928b201f9550354b
Serov Ivan <ISerov@croc.ru>
2022-06-21 20:57:31 +0300

    Добавление решения на задание

---
+++ test_items.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Test_shop():
+
+    @pytest.mark.test_button
+    def test_button(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.implicitly_wait(15)
+        browser.get(link)
+        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "form.add-to-basket#add_to_basket_form button.btn.btn-lg.btn-primary.btn-add-to-basket")))
+        button.click()
+
+
+
