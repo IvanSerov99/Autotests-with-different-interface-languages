176e3d04cde16a4ec50d0e326d4061c509b28f04
Serov Ivan <ISerov@croc.ru>
2022-06-22 12:46:29 +0300

    Исправлен таймер для проверки, и исправлена ошибка default языка в conftest

--- test_items.py
+++ test_items.py
@@ -13,7 +13,7 @@ class Test_shop_functions():
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.implicitly_wait(15)
         browser.get(link)
-        #sleep(30) #sleep для проверки правильности инициализации браузера
+        #time.sleep(30) #sleep для проверки правильности инициализации браузера
         button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
             (By.CSS_SELECTOR, "form.add-to-basket#add_to_basket_form button.btn.btn-lg.btn-primary.btn-add-to-basket")))
         assert (button!= None, 'Кнопка не найдена')
